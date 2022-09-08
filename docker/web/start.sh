3b5d8988fdd80629998e30184316a5ff3f850a39
ccreater222 <ccreater222@gmail.com>
2022-09-08 13:11:25 +0800

    fix(worker): fix "PRECONDITION_FAILED - delivery acknowledgement on channel 1 timed out" error

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 nginx
+touch /tmp/log.txt
 cd /app && nohup ./venv/bin/python app.py run > /tmp/log.txt 2>&1 &
 tail -f /var/log/nginx/access.log & tail -f /var/log/nginx/error.log & tail -f /tmp/log.txt
